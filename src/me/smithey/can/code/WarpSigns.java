73d8cd092414b8e53c9b0b4f987745725be70504
Jordyn Newnham <ttghusky@live.com.au>
2013-02-13 20:14:17 +1100

    Fix of smitheys derpcode

--- src/me/smithey/can/code/WarpSigns.java
+++ src/me/smithey/can/code/WarpSigns.java
@@ -2,7 +2,6 @@ package me.smithey.can.code;
 
 import java.io.File;
 import java.io.IOException;
-
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -14,14 +13,11 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.SignChangeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class WarpSigns extends JavaPlugin implements Listener {
 	public void onEnable() {
-		PluginDescriptionFile pdfFile = this.getDescription();
 		getServer().getPluginManager().registerEvents(this, this);
-		System.out.println( pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!" );
 		try {
 			Metrics metrics = new Metrics(this);
 			metrics.start();
@@ -30,18 +26,13 @@ public class WarpSigns extends JavaPlugin implements Listener {
 		}
 	}
 
-	public void onDisable() {
-		PluginDescriptionFile pdfFile = this.getDescription();
-		System.out.println( pdfFile.getName() + " version " + pdfFile.getVersion() + " is Disabled!" );
-	}
+	public void onDisable() {}
 
 	@EventHandler
 	public void onSignChange(SignChangeEvent e) {
 		Player p = e.getPlayer();
 		if (e.getLine(0).equalsIgnoreCase("[Warp]"))
-			p.sendMessage("Debug: Made sign with [Warp] on line 0");
 			if(p.hasPermission("warpsigns.create")){
-				p.sendMessage("Debug: Has warpsigns.create perm");
 				if (e.getLine(1).isEmpty()) {
 					p.sendMessage(ChatColor.RED + "Please enter a warp name on line 2!");
 					e.setCancelled(true);
@@ -64,24 +55,18 @@ public class WarpSigns extends JavaPlugin implements Listener {
 				Sign s = (Sign)b.getState();
 				Player p = ev.getPlayer();
 				if (s.getLine(0).equalsIgnoreCase("§1[Warp]")){
-					p.sendMessage("Debug: Clicked sign with [Warp] as line 0");
 					if(s.getLine(3).startsWith("{") && s.getLine(3).endsWith("}")) { // Detects if they want to use per sign permission
-						p.sendMessage("Debug: Found PsP on sign");
 						String perm = s.getLine(3).replace("{", ""); // Removes {
 						String fperm = perm.replace("}", ""); // Removes }
 						if(p.hasPermission("WarpSigns.use.*") || p.hasPermission("WarpSigns.use." + fperm)) { // Detects if they have the per sign permission
-							p.sendMessage("Debug: Has perm for sign and warping");
 							ev.getPlayer().performCommand("warp " + s.getLine(1)); // Warps them if they have the per sign perm
 						}else{
-							p.sendMessage("Debug: Dont have perm for PsP for clicked sign");
 							p.sendMessage(ChatColor.RED + "You are missing the permission WarpSigns.use." + fperm);
 						}
-					}else{
-						if (p.hasPermission("warpsigns.use") || p.hasPermission("warpsigns.use.*")){
-							p.sendMessage("Debug: Has warpsigns.use or warpsigns.use.*");
+					} else {
+						if (p.hasPermission("WarpSigns.use") || p.hasPermission("WarpSigns.use.*")){
 							ev.getPlayer().performCommand("warp " + s.getLine(1));
 						} else {
-							p.sendMessage("Debug: No Perm and isnt using PsP");
 							p.sendMessage(ChatColor.RED + "You don't have Permission to use a WarpSign");
 						}
 					}
@@ -89,7 +74,7 @@ public class WarpSigns extends JavaPlugin implements Listener {
 			}
 		}
 	}
-	
+
 	public void createConfig() { // Generate config
 		YamlConfiguration config = YamlConfiguration.loadConfiguration(new File("plugins/WarpSigns/config.yml"));
 		config.set("Enable Per Sign Permission", "true");
